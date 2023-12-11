fc621e46964099c54f14f675b44a2aad5101fa18
Kayr0r404 <mahlarekarabo.702@gmail.com>
2023-12-11 02:11:32 +0200

    false if path not created

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -2,6 +2,7 @@
 # creates and distributes an archive to your web servers
 
 from fabric.api import *
+from os.path import exists
 do_pack = __import__('1-pack_web_static').do_pack
 do_deploy = __import__('2-do_deploy_web_static').do_deploy
 env.hosts = ['100.25.136.187', '54.236.50.4']
@@ -13,7 +14,7 @@ def deploy():
     """Create and distribute an archive to web servers"""
     archive_path = do_pack()
 
-    if not archive_path:
+    if not exists(archive_path):
         return False
 
     return do_deploy(archive_path)
