0647924f2ebd5aaae06e426d5e0d317bb3f6e7a3
Kayr0r404 <mahlarekarabo.702@gmail.com>
2023-12-25 12:16:27 +0200

    py

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+'''script that starts a Flask web application'''
+
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    '''listening on 0.0.0.0, port 5000'''
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    text = text.replace('_', ' ')
+    return 'C {}'.format(escape(text))
+
+@app.route('/python/<text>', strict_slashes=False)
+def python_is_cool(text='is cool'):
+    return 'Python {}'.format(escape(text))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
