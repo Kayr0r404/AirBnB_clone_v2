a39754696b6bd289e6a92eb458d7d51a987ee163
Kayr0r404 <mahlarekarabo.702@gmail.com>
2023-12-26 13:16:37 +0200

    is number'

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+'''script that starts a Flask web application'''
+
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    '''listening on 0.0.0.0, port 5000'''
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    text = text.replace('_', ' ')
+    return 'C {}'.format(escape(text))
+
+
+@app.route('/python/<text>', strict_slashes=False)
+@app.route('/python/', strict_slashes=False)
+def python_route(text='is_cool'):
+    text = text.replace('_', ' ')
+    return 'Python {}'.format(text)
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def is_number(n):
+    return '{} is a number'.format(escape(n))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
