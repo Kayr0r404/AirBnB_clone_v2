bad8c3a2e1b91081cb99c226d627f52f670be6ed
Kayr0r404 <mahlarekarabo.702@gmail.com>
2024-01-02 04:47:10 +0200

    flake compliant

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -4,15 +4,18 @@ from models import storage
 
 app = Flask(__name__)
 
+
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     states = storage.all("State")
     return render_template("7-states_list.html", states=states)
 
+
 @app.teardown_appcontext
 def close_session(exception):
     '''remove the current SQLAlchemy Session'''
     storage.close()
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
