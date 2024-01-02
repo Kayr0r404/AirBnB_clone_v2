fd3670b8997ab06600dd78a26b995acea9f9447c
Kayr0r404 <mahlarekarabo.702@gmail.com>
2024-01-02 04:45:16 +0200

    ren

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,19 +1,16 @@
-#!/usr/bin/pythin3
+#!/usr/bin/python3
 from flask import Flask, render_template
-from markupsafe import escape
 from models import storage
-
-dict = storage.all('State')
 
 app = Flask(__name__)
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
-    return render_template('7-states_list.html', states=states)
+    states = storage.all("State")
+    return render_template("7-states_list.html", states=states)
 
 @app.teardown_appcontext
-def close_session():
+def close_session(exception):
     '''remove the current SQLAlchemy Session'''
     storage.close()
 
